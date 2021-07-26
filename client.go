355d1140f3a3dc305c4f41181050942448a88cd3
mayb <mayb@mayb.cn>
2021-07-26 14:55:57 +0800

    update

--- client.go
+++ client.go
@@ -6,15 +6,19 @@ import (
 	"time"
 
 	"github.com/allegro/bigcache/v3"
-	"github.com/gogrpc/glog"
 	"github.com/gogrpc/config/proto"
-	"google.golang.org/grpc"
+	"github.com/gogrpc/glog"
+	"github.com/gogrpc/pool"
 )
 
-var cache *bigcache.BigCache
+var (
+	cache *bigcache.BigCache
+	pol   pool.Pool
+)
 
 func init() {
 	cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(time.Second * 60))
+	pol, _ = pool.New([]string{App.AppConfigService.Address}, pool.DefaultOptions)
 }
 
 //根据key获取配置信息
@@ -25,7 +29,6 @@ func Get(key string) (string, error) {
 	}
 
 	client := getClient()
-	defer client.close()
 	out, err := client.RPC.GetConfigValue(client.Ctx, &proto.ConfigRequest{
 		AppName:     App.AppConfigService.Appid,
 		EnvCode:     os.Getenv("ASPNETCORE_ENVIRONMENT"),
@@ -51,28 +54,15 @@ func GetString(key string) string {
 }
 
 type configClient struct {
-	Conn *grpc.ClientConn
-	Ctx  context.Context
-	Cf   context.CancelFunc
-	RPC  proto.AppConfigClient
+	Ctx context.Context
+	Cf  context.CancelFunc
+	RPC proto.AppConfigClient
 }
 
 func getClient() *configClient {
 	var client configClient
-	var err error
-	if client.Conn, err = grpc.Dial(App.AppConfigService.Address, grpc.WithInsecure(), grpc.WithBlock()); err != nil {
-		glog.Error(err)
-		return nil
-	} else {
-		client.RPC = proto.NewAppConfigClient(client.Conn)
-		client.Ctx = context.Background()
-		client.Ctx, client.Cf = context.WithTimeout(client.Ctx, time.Second*5)
-		return &client
-	}
-}
-
-//关闭链接
-func (c *configClient) close() {
-	c.Conn.Close()
-	c.Cf()
+	client.RPC = proto.NewAppConfigClient(pol.GetConn().Value())
+	client.Ctx = context.Background()
+	client.Ctx, client.Cf = context.WithTimeout(client.Ctx, time.Second*5)
+	return &client
 }
