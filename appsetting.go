90ab2c0fc399907d2cca5208073e10e5ddcfaf56
mayb <mayb@mayb.cn>
2021-11-03 10:58:46 +0800

    update

--- appsetting.go
+++ appsetting.go
@@ -17,12 +17,11 @@ type AppSetting struct {
 }
 
 func init() {
+	glog.Info("AppConfigService init...")
+	glog.Info(App.ConfigService.Appid, App.ConfigService.Address)
 	//初始化配置
 	_, err := toml.DecodeFile("appsetting.toml", &App)
 	if err != nil {
 		glog.Error(err)
-		log.Fatal(err)
 	}
-	glog.Info("AppConfigService init...")
-	glog.Info(App.ConfigService.Appid, App.ConfigService.Address)
 }
